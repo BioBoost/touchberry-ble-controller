ce897207f62333cd88185ff90b49a9773c5546b4
Nico De Witte <nico.dewitte@vives.be>
2019-11-20 20:42:19 +0000

    Catch kill signals for graceful shutdown

--- src/controller.js
+++ src/controller.js
@@ -6,3 +6,15 @@ console.log("Welcome to TouchBerry BLE controller");
 let config = JSON.parse(fs.readFileSync('./config.json'));
 
 let controller = new TouchberryController(config.id, config.color);
+
+function shutdown() {
+  console.log('Shutting down ble touchberry controller');
+  controller.stop();
+  console.log("Done ...");
+  process.exit(0);
+}
+
+// Gracefull shutdown
+process.once('SIGINT', shutdown);   // CTRL-C
+process.once('SIGTERM', shutdown);  // Request to terminate (send by for example docker)
+
