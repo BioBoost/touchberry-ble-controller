02c6cca548fdd7c0dc98f39cbb5e43b486459117
Nico De Witte <nico.dewitte@vives.be>
2019-11-20 12:33:26 +0000

    Add configurable id and color

--- src/controller.js
+++ src/controller.js
@@ -1,5 +1,8 @@
 const TouchberryController = require('./touchberry_controller');
+const fs = require('fs');
 
 console.log("Welcome to TouchBerry BLE controller");
 
-let controller = new TouchberryController();
+let config = JSON.parse(fs.readFileSync('./config.json'));
+
+let controller = new TouchberryController(config.id, config.color);
--- src/touchberry_controller.js
+++ src/touchberry_controller.js
@@ -1,9 +1,12 @@
 const TouchBerry = require('touchberry');
 const BLEDevice = require('./ble_device');
+const Color = require('color');
 
 class TouchBerryController {
 
-  constructor() {
+  constructor(id, color) {
+    this.id = id;
+    this.color = Color(color);
     this.initialize_shield();
     this.initialize_ble_device();
   }
@@ -11,6 +14,12 @@ class TouchBerryController {
   initialize_shield() {
     this.shield = TouchBerry.BoardBuilder.build();
     console.log(`Detected ${this.shield.revision()} board`);
+
+    // Refactor later to nicer code
+    this.shield.rgbledbar().all_off();
+    for (let i = 0; i < 5; i++) {
+      this.shield.rgbledbar().set_led(i, this.color);
+    }
   }
 
   initialize_ble_device() {
